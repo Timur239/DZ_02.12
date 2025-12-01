62a5cc3a64443044f18a5cda86e7e9d14c485af1
Timur239 <147524958+Timur239@users.noreply.github.com>
2025-12-01 20:17:54 +0300

    Update MessageController.java

--- demo/src/main/java/com/example/demo/MessageController.java
+++ demo/src/main/java/com/example/demo/MessageController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 @Controller
@@ -13,10 +14,10 @@ import java.util.stream.Collectors;
 public class MessageController {
     private final List<String> messages = new ArrayList<>();
     // curl -X GET http://localhost:8080/api/messages
-    @GetMapping("messages")
+    /*@GetMapping("messages")
     public ResponseEntity<List<String>> getMessages() {
         return ResponseEntity.ok(messages);
-    }
+    }*/
     // curl -X POST http://localhost:8080/api/messages -H "Content-Type: text/plain" -d "Hello World"
     @PostMapping("messages")
     public ResponseEntity<Void> addMessage(@RequestBody String text) {
@@ -45,4 +46,51 @@ public class MessageController {
         messages.add(i, message);
         return ResponseEntity.accepted().build();
     }
+    // curl -X GET http://localhost:8080/api/messages/search/text
+    @GetMapping("messages/search/{text}")
+    public ResponseEntity<Integer> aboba(@PathVariable("text") String
+                                                     text) {
+        for(int i = 0; i < messages.size(); i++){
+            if (messages.get( i ).contains( text )){
+                return ResponseEntity.ok(i);
+            }
+        }
+        return ResponseEntity.ok(0);
+    }
+    // curl -X GET http://localhost:8080/api/messages/count
+    @GetMapping("messages/count")
+    public ResponseEntity<Integer> space() {
+        return ResponseEntity.ok(messages.size());
+    }
+    // curl -X POST http://localhost:8080/api/messages/1480/create
+    @PostMapping("messages/{index}/create")
+    public ResponseEntity<Void> guest(@PathVariable("index") Integer index) {
+        messages.add(index, index.toString());
+        return ResponseEntity.accepted().build();
+    }
+    // curl -X DELETE http://localhost:8080/api/messages/search/text
+    @DeleteMapping("messages/search/{text}")
+    public ResponseEntity<Void> world(@PathVariable("text") String
+                                                 text) {
+        for(int i = 0; i < messages.size(); i++){
+            if (messages.get( i ).contains( text )){
+                messages.remove(i);
+                i -= 1;
+            }
+        }
+        return ResponseEntity.noContent().build();
+    }
+    // curl -X GET http://localhost:8080/api/messages -H "Content-type: application/json" -d "Text2"
+    @GetMapping("messages")
+    public ResponseEntity<List<String>> water(@RequestBody String
+                                                          text) {
+        LinkedList<String> otvet = new LinkedList<>();
+        for ( int i = 0; i < messages.size(); i++ ){
+            if ( messages.get(i).startsWith(text) ){
+                otvet.add(messages.get(i));
+            }
+        }
+        return ResponseEntity.ok(otvet);
+    }
 }
+
